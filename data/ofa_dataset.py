da58e6d53718818b2710857da8bb415747ac31ea
seoilgun <seoilgun@gmail.com>
2022-07-27 12:25:41 +0900

    ha

--- data/ofa_dataset.py
+++ data/ofa_dataset.py
@@ -28,10 +28,10 @@ class OFADataset(FairseqDataset):
     def __len__(self):
         return len(self.dataset)
 
-    def encode_text(self, text, length=None, append_bos=False, append_eos=False, use_bpe=True):
+    def encode_text(self, text, length=None, append_bos=False, append_eos=False, use_bpe=True, add_if_not_exist=False):
         s = self.tgt_dict.encode_line(
             line=self.bpe.encode(text) if use_bpe else text,
-            add_if_not_exist=False,
+            add_if_not_exist=add_if_not_exist,
             append_eos=False
         ).long()
         if length is not None:
