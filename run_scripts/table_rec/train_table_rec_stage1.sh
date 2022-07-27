e417b86f27a4a27b9d2b79e75e857141c7cf6b77
seoilgun <seoilgun@gmail.com>
2022-07-27 19:42:23 +0900

    ha

--- run_scripts/table_rec/train_table_rec_stage1.sh
+++ run_scripts/table_rec/train_table_rec_stage1.sh
@@ -11,8 +11,8 @@ mkdir -p $log_dir $save_dir
 bpe_dir=../../utils/BPE
 user_dir=../../ofa_module
 
-data_dir=../../dataset/caption_data
-data=${data_dir}/caption_stage1_train.tsv,${data_dir}/caption_val.tsv
+data_dir=/home/ubuntu/dataset/table_ocr/pubtabnet/ofa_dataset
+data=${data_dir}/ofa_dataset_train.tsv,${data_dir}/ofa_dataset_val.tsv
 restore_file=../../checkpoints/ofa_large.pt
 selected_cols=0,1,2
 
