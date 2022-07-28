dbb8d6db625dd3f569bccc137924218e12e78a82
seoilgun <seoilgun@gmail.com>
2022-07-28 14:13:47 +0900

    ha

--- models/ofa/unify_transformer.py
+++ models/ofa/unify_transformer.py
@@ -592,6 +592,8 @@ class TransformerEncoder(FairseqEncoder):
         # embed raw images
         if image_embed is not None:
             logger.info(image_embed.shape)
+            logger.info(image_embed.min())
+            logger.info(image_embed.max())
             image_embed = self.image_proj(image_embed)
             raise Exception("debug end")
             image_x = image_embed = self.embed_scale * image_embed
